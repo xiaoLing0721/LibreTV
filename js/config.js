e4c9508a9a571177c5fe3339bb94a08b1ec8c44c
xiaoLing0721 <xiaoling@duck.com>
2025-05-28 23:30:09 +0800

    auto commit

--- js/config.js
+++ js/config.js
@@ -12,11 +12,11 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
-    url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    name: 'onlTV',
+    url: 'https://www.onltv.qzz.io/',
+    description: 'onlTV - 免费在线视频搜索与观看平台',
     logo: 'image/logo.png',
-    version: '1.0.3'
+    version: '1.0.4'
 };
 
 // API站点配置
